1802f7608263b1201fa7a17f84bb5eb90ffb40ce
muhhand <mcmohand888@gmail.com>
2022-07-13 23:36:31 +0200

    Bmi Calculator

---
+++ lib/bmi_result/bmi_result_screen.dart
@@ -0,0 +1,59 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BmiResultScreen extends StatelessWidget {
+
+  final int result;
+  final bool isMale;
+  final int age;
+
+  BmiResultScreen({
+     required this.result,
+     required this.age,
+     required this.isMale,
+
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+        Text(
+          'Bmi Result'
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Gender : ${isMale ? 'Male' : 'Female' }',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Result : ${result}',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Age : ${age}',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
